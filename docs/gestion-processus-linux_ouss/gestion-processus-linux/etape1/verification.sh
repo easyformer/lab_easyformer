786ccab4c6c529d2fe3ffc625d024d323abdf2e6
0u55 <53933154+saitama142@users.noreply.github.com>
2025-04-17 16:06:51 +0200

    Delete docs/gestion-processus-linux_ouss/gestion-processus-linux directory

--- docs/gestion-processus-linux_ouss/gestion-processus-linux/etape1/verification.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ps aux | grep -q '[s]ystemd'; then
-    echo "done"
-    exit 0
-else
-    echo "Le processus systemd n'est pas trouvé"
-    exit 1
-fi
--- docs/gestion-processus-linux_ouss/gestion-processus-linux/etape3/verification.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if systemctl is-enabled ssh | grep -q enabled; then
-    echo "done"
-    exit 0
-else
-    echo "Le service SSH n'est pas activé"
-    exit 1
-fi
