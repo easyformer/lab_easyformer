9bb99b0fea593cf50a68fa566bdc5a71a8fc9871
0u55 <53933154+saitama142@users.noreply.github.com>
2025-04-17 16:17:34 +0200

    Update verification.sh

--- TP-ouss/gestion-processus-linux/etape3/verification.sh
+++ TP-ouss/gestion-processus-linux/etape3/verification.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if systemctl is-enabled ssh | grep -q enabled; then
+if systemctl is-active cron | grep -q active && \
+   systemctl is-enabled cron | grep -q enabled; then
     echo "done"
     exit 0
 else
-    echo "Le service SSH n'est pas activé"
+    echo "Le service Cron n'est pas correctement configuré"
     exit 1
 fi
